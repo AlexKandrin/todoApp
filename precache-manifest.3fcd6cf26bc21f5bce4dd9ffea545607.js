0a860b45c02ce6c12ba2505c48d66fa4e6ed8f88
Saha <sashakandrin@gmail.com>
2020-05-21 08:25:04 +0300

    Updates

---
+++ precache-manifest.3fcd6cf26bc21f5bce4dd9ffea545607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a3a4166279a3608d7f71fafe75a2b5",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "91328fcefdf3705f5784",
+    "url": "/todoApp/static/css/main.e2c9528b.chunk.css"
+  },
+  {
+    "revision": "becc3f17a3f7e150cc26",
+    "url": "/todoApp/static/js/2.60340745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.60340745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91328fcefdf3705f5784",
+    "url": "/todoApp/static/js/main.34d39a23.chunk.js"
+  },
+  {
+    "revision": "109980f21ca187639c38",
+    "url": "/todoApp/static/js/runtime-main.adea7fd8.js"
+  }
+]);
